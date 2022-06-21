aeaf7cd5b9d8684900c04a2047a3c810b5b747b6
Alberto <aserranor@cifpfbmoll.eu>
2022-06-21 21:02:23 +0200

    Creada la clase AudioController
    Se utiliza la clase CLIP para la reproduccion de audio y los audios debemn ser .WAV

--- src/AudioServer/AudioController.java
+++ src/AudioServer/AudioController.java
@@ -3,12 +3,63 @@ package AudioServer;
 import communications.CommunicationController;
 import communications.ProtocolDataPacket;
 
-import java.sql.Connection;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import java.io.File;
 
 public class AudioController {
-    CommunicationController communicationController = new CommunicationController();
 
-    public void selectSong(ProtocolDataPacket packet){
+    private CommunicationController communicationController = new CommunicationController();
+    private AudioInputStream audioInputStream;
+    private Clip clip;
+    private String filePath;
 
+    public AudioController(String path){
+        this.filePath = path;
+        try {
+            clip = AudioSystem.getClip();
+        }catch (LineUnavailableException e){
+            e.printStackTrace();
+        }
+    }
+
+    public AudioController loadAudio(){
+
+        try{
+            File audioFile = new File(filePath);
+            if (audioFile.exists()){
+                audioInputStream = AudioSystem.getAudioInputStream(audioFile);
+                clip.open(audioInputStream);
+            }else {
+                System.out.println("No se ha podido cargar el audio");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return this;
+    }
+
+    public AudioController playMusic(){
+        clip.start();
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+        return this;
+    }
+
+    public AudioController playSounds(){
+        clip.start();
+        return this;
+    }
+    public AudioController pause(){
+        clip.stop();
+        return this;
+    }
+
+    public static void main(String[] args){
+        AudioController audio = new AudioController("C:\\Users\\Alberturris\\Documents\\IntelliJProjects\\RasteroidSoundServer\\src\\AudioServer\\Resources\\theGrandFinale.wav");
+        audio.loadAudio();
+        audio.playSounds();
     }
 }
