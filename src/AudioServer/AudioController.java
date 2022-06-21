78ad2f09fdb2f155560e454a45774eb542bd2903
David <dsastref@cifpfbmoll.eu>
2022-06-21 20:31:58 +0200

    Created Class Audio Controller

---
+++ src/AudioServer/AudioController.java
@@ -0,0 +1,14 @@
+package AudioServer;
+
+import communications.CommunicationController;
+import communications.ProtocolDataPacket;
+
+import java.sql.Connection;
+
+public class AudioController {
+    CommunicationController communicationController = new CommunicationController();
+
+    public void selectSong(ProtocolDataPacket packet){
+
+    }
+}
